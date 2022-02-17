60ef1b3a3e6142e8eabb8d98a748eb9938f1851e
celebrater <tkgm45nbchk@gmail.com>
2022-02-18 05:38:51 +0900

    test succeeded again

--- main.js
+++ main.js
@@ -3,18 +3,18 @@
 let int1 = 0;
 let int2 = 0;
 
+// int1とint2を足し算する関数
+const sum_int = () => {
+    console.log(int1 + int2);
+}
+
 // int1とint2に値をセットする関数
-function set_int(a, b, callback){
+const set_int = (a, b, callback) => {
     setTimeout(function(){
         int1 = a;
         int2 = b;
         callback();
     }, 1000);
-}
-
-// int1とint2を足し算する関数
-function sum_int(){
-    alert(int1 + int2);
 }
 
 // set_int(1, 2, sum_int);
